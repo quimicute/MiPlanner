46f884c333e6ce3ef415dec0afc7e6a412ee62b6
quimicute <mychemicalwoman@gmail.com>
2026-04-21 14:33:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -923,7 +923,6 @@
                                 else if (t.enmsMateria === 'Química II') dotColor = '#FFB745'; // Amarillo
                                 
                                 return `<div style="width: 10px; height: 10px; background: ${dotColor}; border-radius: 50%; display: inline-block; margin: 2px; box-shadow: 0 1px 3px rgba(0,0,0,0.1);" title="${t.title}" onclick="openTaskDetails(${t.id}); event.stopPropagation();"></div>`;
-                            }
                             
                             // Lógica normal para los demás eventos (cumples, etc)
                             let sym = '';
